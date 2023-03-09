9c4204ebaf5d97ff1989cb09f66fcd1894317cff
Austin Woods <austinwoods99@gmail.com>
2023-03-09 09:52:58 -0500

    Partially complete all_patern_spec

--- ruby_exercises/enumerables/exercises_1/spec/all_pattern_spec.rb
+++ ruby_exercises/enumerables/exercises_1/spec/all_pattern_spec.rb
@@ -8,31 +8,36 @@ RSpec.describe 'All Pattern' do
     expect(all_zeros).to eq(true)
   end
 
-  xit 'not all zeros' do
+  it 'not all zeros' do
     numbers = [0, 0, 0, 0, 1, 0, 0, 0]
     all_zeros = true
     numbers.each do |number|
-      # Your code goes here
+      all_zeros = false unless number.zero?
     end
     expect(all_zeros).to eq(false)
   end
 
-  xit 'all gone' do
+  it 'all gone' do
     words = ["gone", "gone", "gone", "gone", "gone", "gone", "gone"]
     all_gone = true
-    # Your code goes here
+    words.each do |word|
+      all_gone = false unless word.include?("gone")
+    end
     expect(all_gone).to eq(true)
   end
 
-  xit 'not all gone' do
+  it 'not all gone' do
     words = ["gone", "gone", "gone", "gone", "gone", "there", "gone", "gone"]
-    # Your code goes here
+    all_gone = true
+    words.each do |word|
+      all_gone = false unless word.include?("gone")
+    end
     expect(all_gone).to eq(false)
   end
 
   xit 'all empty' do
     strings = ["", "", "", "", "", "", ""]
-    # Your code goes here
+    # Your code goes heres
     expect(all_empty).to eq(true)
   end
 
